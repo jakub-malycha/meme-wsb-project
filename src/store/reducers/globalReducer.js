67c74661ccd96a5f41b20ad0d41c5f016d84e786
jakub-malycha <jakub.malycha1@gmail.com>
2021-06-10 09:06:25 +0200

    adding new memes

--- src/store/reducers/globalReducer.js
+++ src/store/reducers/globalReducer.js
@@ -28,6 +28,48 @@ const initialState = {
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSMoip-UsqU2kynAUWpgA7_Eta3sU63yzeNMA&usqp=CAU",
       id: 4,
     },
+    {
+      title: "Mem 5",
+      upvotes: 11,
+      downvotes: 12,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/06/qd4oPbiZzXoZ29GN.jpg",
+      id: 5,
+    },
+    {
+      title: "Mem 6",
+      upvotes: 6,
+      downvotes: 8,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/06/cBqp9S5COMu3IruO.jpg",
+      id: 6,
+    },
+    {
+      title: "Mem 7",
+      upvotes: 21,
+      downvotes: 9,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/06/lyCrOE66NKAREbno.jpg",
+      id: 7,
+    },
+    {
+      title: "Mem 8",
+      upvotes: 54,
+      downvotes: 23,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/01/ip0gbo6rFW68RMJB.jpg",
+      id: 8,
+    },
+    {
+      title: "Mem 9",
+      upvotes: 31,
+      downvotes: 22,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/05/0m5F7PFBURHISgXd.jpg",
+      id: 9,
+    },
+    {
+      title: "Mem 10",
+      upvotes: 11,
+      downvotes: 12,
+      img: "https://i1.kwejk.pl/k/obrazki/2021/06/ILbYbFpg6QKW1z6Y.jpg",
+      id: 10,
+    },
   ],
 };
 
