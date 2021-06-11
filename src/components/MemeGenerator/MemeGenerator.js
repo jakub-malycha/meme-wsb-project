f93597d2d25647057de0d9060ca3b125b1679332
jakub-malycha <jakub.malycha1@gmail.com>
2021-06-11 11:47:09 +0200

    added generate meme feature

--- src/components/MemeGenerator/MemeGenerator.js
+++ src/components/MemeGenerator/MemeGenerator.js
@@ -8,6 +8,47 @@ const MemeGenerator = (props) => {
   //   const memes = useSelector((state) => state.memes);
   const [fetchedMemes, setFetchedMemes] = useState([]);
   const [memeIndex, setMemeIndex] = useState(0);
+  const [captions, setCaptions] = useState([]);
+
+  const updateCaption = (e, index) => {
+    const text = e.target.value || "";
+    setCaptions(
+      captions.map((caption, i) => {
+        if (index === i) {
+          return text;
+        } else {
+          return caption;
+        }
+      })
+    );
+  };
+
+  const generateMeme = () => {
+    const currentMeme = fetchedMemes[memeIndex];
+    const formData = new FormData();
+
+    formData.append("username", "qba1234");
+    formData.append("password", "PaSS1234");
+    formData.append("template_id", currentMeme.id);
+    captions.forEach((caption, index) =>
+      formData.append(`boxes[${index}][text]`, caption)
+    );
+
+    fetch("https://api.imgflip.com/caption_image", {
+      method: "POST",
+      body: formData,
+    }).then((res) => {
+      res.json().then((res) => {
+        const memeUrl = res.data.url;
+        //  history.push(`/generated?url=${res.data.url}`);
+        console.log(memeUrl);
+        return (currentMeme.img = memeUrl);
+      });
+    });
+    // props.update(fetchedMemes[memeIndex]);
+    props.update(currentMeme);
+    setMemeIndex(memeIndex + 1);
+  };
 
   useEffect(() => {
     axios.get("https://api.imgflip.com/get_memes").then(
@@ -39,14 +80,15 @@ const MemeGenerator = (props) => {
     }
   };
 
-  const updateMemesAndSkip = () => {
-    props.update(fetchedMemes[memeIndex]);
-    setMemeIndex(memeIndex + 1);
-  };
+  useEffect(() => {
+    if (fetchedMemes.length) {
+      setCaptions(Array(fetchedMemes[memeIndex].box_count).fill(""));
+    }
+  }, [memeIndex, fetchedMemes]);
 
   return fetchedMemes.length ? (
     <div className={classes.container}>
-      <button onClick={updateMemesAndSkip} className={classes.generate}>
+      <button onClick={generateMeme} className={classes.generate}>
         Generate
       </button>
       <button
@@ -55,6 +97,9 @@ const MemeGenerator = (props) => {
       >
         Skip
       </button>
+      {captions.map((c, index) => (
+        <input onChange={(e) => updateCaption(e, index)} key={index} />
+      ))}
       <img src={fetchedMemes[memeIndex].img} />
     </div>
   ) : (
